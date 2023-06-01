17ce9747c5194c2b2f4246f1cbef1acf16c8fa25
Erdem Ersoy <erdemersoy256@gmail.com>
2023-06-01 15:26:23 +0300

    Make initial desktop routine

--- src/main.v
+++ src/main.v
@@ -5,6 +5,27 @@
 
 module main
 
+import gg
+import gx
+
 fn main() {
 	println("${project_name} ${version}")
+	println("Initializing the desktop...")
+
+	mut context := gg.new_context(
+		bg_color: gx.black
+		width: 800
+		height: 600
+		window_title: "${project_name} ${version}"
+		frame_fn: veinbox_frame
+	)
+	context.run()
+
+	println("Simulation finished!")
+}
+
+fn veinbox_frame(mut ctx gg.Context)
+{
+	ctx.begin()
+    ctx.end()
 }
