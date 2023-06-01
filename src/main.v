d47b710b943ddc3d69eabd16ec0ba07359e2c96b
Erdem Ersoy <erdemersoy256@gmail.com>
2023-06-01 15:15:52 +0300

    Initial commit

---
+++ src/main.v
@@ -0,0 +1,5 @@
+module main
+
+fn main() {
+	println("${project_name} ${version}")
+}
---
+++ src/version.v
@@ -0,0 +1,9 @@
+module main
+
+const project_name   = "VeinBox"
+
+const version_major  = u8(0)
+const version_middle = u8(0)
+const version_minor  = u8(1)
+
+const version        = "${version_major}.${version_middle}.${version_minor}"
